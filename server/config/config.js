c6708392371283e4aa04393b3d7b166774665be5
Judith <judith.costa@eurobikes.com>
2020-11-03 17:51:58 +0100

    RestServer - usuario modificación pq la config de url estaba comentada

--- server/config/config.js
+++ server/config/config.js
@@ -9,10 +9,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; // si la variante no exist
 
 //? =========== BASE DE DATOS =============
 let urlDB;
-/* if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe-cursonode';
 } else {
     urlDB = 'mongodb+srv://judcsaura:C9uXq3ceIuvL7wXZ@cluster0.cjisb.mongodb.net/cafe';
-} */
-urlDB = 'mongodb+srv://judcsaura:C9uXq3ceIuvL7wXZ@cluster0.cjisb.mongodb.net/cafe';
-process.env.URLDB = urlDB;
+}
+// urlDB = 'mongodb+srv://judcsaura:C9uXq3ceIuvL7wXZ@cluster0.cjisb.mongodb.net/cafe';
+// process.env.URLDB = urlDB;
