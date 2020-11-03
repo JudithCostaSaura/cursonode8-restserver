9285fc0c59a26f2a6b33864ca00719c57d9c5862
Judith <judith.costa@eurobikes.com>
2020-11-03 19:22:00 +0100

    con nuevo usuario en fin

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe-cursonode';
 } else {
-    urlDB = 'mongodb+srv://judcsaura:C9uXq3ceIuvL7wXZ@cluster0.cjisb.mongodb.net/cafe';
+    // urlDB = 'mongodb+srv://judcsaura:C9uXq3ceIuvL7wXZ@cluster0.cjisb.mongodb.net/cafe';
+    urlDB = 'mongodb+srv://judith2:Not33SinThem34@cluster0.cjisb.mongodb.net/cafe';
 }
 // comentario de a ver si va ya esta puta mierda
 process.env.URLDB = urlDB;
